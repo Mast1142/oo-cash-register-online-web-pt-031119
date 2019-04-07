66fd578c8cbb8b30d0c96f1c094a1bbc4e92d6b0
Samuel T <ssamt1142@gmail.com>
2019-04-07 18:51:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,10 @@ attr_accessor :total, :discount
   end
 
   def apply_discount
-    @total = @total*((100-@discount)/100.0)
+    if discount > 0
+      @total = @total*((100-@discount)/100.0)
+      "After the discount, the total comes to #{@total}"
+
     #binding.pry
   end
 
