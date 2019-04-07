46dc859f4cbd1d667e03cb3df110010ff48847d2
Samuel T <ssamt1142@gmail.com>
2019-04-07 18:14:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+require "pry"
 
+class CashRegister
+
+attr_accessor :total, :discount
+
+  def initialize(total = 0)
+    @total = total
+    @discount = 20
+  end
+
+  def total
+    @total = total
+  end
+
+
+end
